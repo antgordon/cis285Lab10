a31376536d76e250388fbf3313c2a7ce3f07d452
Antony Gordon <gordonad@umich.edu>
2020-12-02 20:24:38 -0500

    Setup duplicate and mixed tests

--- src/code/testSelectionSort.java
+++ src/code/testSelectionSort.java
@@ -2,13 +2,14 @@ package code;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
 
 	
 
-	@Test
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -27,7 +28,7 @@ public class testSelectionSort {
         /** add tests to check for this unit test **/
     }
     
-	@Test
+	
     public void testNegative(){
         
         /** Test data contains negative values only **/
@@ -36,13 +37,50 @@ public class testSelectionSort {
 	@Test
     public void testMixed(){
         
-        /** Test data contains with both positive, negative and zeros **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+        arr[1] = 9;
+        arr[2] = 0;
+        arr[3] = 10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -8;
+        Sortedarr[1] = 0;
+        Sortedarr[2] = 2;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+        
+        
+        SelectionSort sort = new SelectionSort();
+        sort.basicSelectionSort(arr);
+        
+        Assert.assertArrayEquals(Sortedarr, arr);
     }
     
 	@Test
     public void testDuplicates(){
         
-        /** Test data contains duplicates **/
+		int[] arr = new int[5];
+		arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 9;
+        arr[3] = 10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 8;
+        Sortedarr[2] = 9;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+        
+        
+        SelectionSort sort = new SelectionSort();
+        sort.basicSelectionSort(arr);
+        
+        Assert.assertArrayEquals(Sortedarr, arr);
+	        
     }
 
 
