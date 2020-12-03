67121874d48049c39c1189065b25527a17145565
Antony Gordon <gordonad@umich.edu>
2020-12-02 20:00:44 -0500

    Add positive and negative tests

--- src/code/testSelectionSort.java
+++ src/code/testSelectionSort.java
@@ -2,6 +2,7 @@ package code;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
@@ -24,22 +25,43 @@ public class testSelectionSort {
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
         
-        /** add tests to check for this unit test **/
+        SelectionSort sort = new SelectionSort();
+        sort.basicSelectionSort(arr);
+        
+
+        Assert.assertArrayEquals(Sortedarr, arr);
     }
     
 	@Test
     public void testNegative(){
+	    int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
         
-        /** Test data contains negative values only **/
-    }
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        SelectionSort sort = new SelectionSort();
+        sort.basicSelectionSort(arr);
+        
+
+        Assert.assertArrayEquals(Sortedarr, arr);
+}
     
-	@Test
+
     public void testMixed(){
         
         /** Test data contains with both positive, negative and zeros **/
     }
     
-	@Test
+
     public void testDuplicates(){
         
         /** Test data contains duplicates **/
